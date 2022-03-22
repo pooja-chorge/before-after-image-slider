e9777ece89015dd84ba54e3f27b455f253fd340c
pooja <pooja.chorge@futurescapetech.com>
2022-03-23 00:04:40 +0530

    modification in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-const slider = document.querySelector(".slider input");
-const img = document.querySelector(".images .colorfull-img");
-const sliderLine = document.querySelector(".slider .slider-line");
+const slider = document.querySelector("input");
+const img = document.querySelector(".colorfull-img");
+const sliderLine = document.querySelector(".slider-line");
 
 slider.oninput = () =>{
     let sliderVal = slider.value;
